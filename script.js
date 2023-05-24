24b4d4b887635c019d4f9f84c053560add48a029
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-05-24 16:30:20 +0500

    Complete mobile menu popup section and remove eslint issues for linters

--- script.js
+++ script.js
@@ -1,61 +1,78 @@
 const speakers = {
-    speaker1: {
-        name:'Kamran Asif',
-        title:'Software Engineer',
-        image: 'images/My project-1.png',
-        chess_icon:'images/chess.jpg',
-        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?'
-    },
-    speaker2: {
-        name:'Rana Mudassir',
-        title:'Software Engineer',
-        image: 'images/mudassir.png',
-        chess_icon:'images/chess.jpg',
-        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?'
-    },
-    speaker3: {
-        name:'Mir Hamza',
-        title:'Software Engineer',
-        image: 'images/pachal.jpg',
-        chess_icon:'images/chess.jpg',
-        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?'
-    },
-    speaker4: {
-        name:'Zeeshan',
-        title:'Software Engineer',
-        image: 'images/hamza.jpg',
-        chess_icon:'images/chess.jpg',
-        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?'
-    },
-    speaker5: {
-        name:'Aarif Hameed',
-        title:'Software Engineer',
-        image: 'images/zain.jpg',
-        chess_icon:'images/chess.jpg',
-        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?'
-    },
-    speaker6: {
-        name:'Zahid Akbar',
-        title:'Software Engineer',
-        image: 'images/zeeshan.jpg',
-        chess_icon:'images/chess.jpg',
-        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?'
-    },
-}
-let speakers_html ='';
-// console.log(Object.(speakers));
+  speaker1: {
+    name: 'Kamran Asif',
+    title: 'Software Engineer',
+    image: 'images/My project-1.png',
+    chessIcon: 'images/chess.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
+  },
+  speaker2: {
+    name: 'Rana Mudassir',
+    title: 'Software Engineer',
+    image: 'images/mudassir.png',
+    chessIcon: 'images/chess.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
+  },
+  speaker3: {
+    name: 'Mir Hamza',
+    title: 'Software Engineer',
+    image: 'images/pachal.jpg',
+    chessIcon: 'images/chess.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
+  },
+  speaker4: {
+    name: 'Zeeshan',
+    title: 'Software Engineer',
+    image: 'images/hamza.jpg',
+    chessIcon: 'images/chess.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
+  },
+  speaker5: {
+    name: 'Aarif Hameed',
+    title: 'Software Engineer',
+    image: 'images/zain.jpg',
+    chessIcon: 'images/chess.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
+  },
+  speaker6: {
+    name: 'Zahid Akbar',
+    title: 'Software Engineer',
+    image: 'images/zeeshan.jpg',
+    chessIcon: 'images/chess.jpg',
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque tenetur quaerat repellat sint doloribus numquam hic!Fugiat illo at eligendi a nihil ullam eos voluptatem nesciunt?',
+  },
+};
+const humbergIcon = document.getElementById('navbar-btn');
+const closeBtn = document.getElementById('close_nav');
+const navbarNav = document.getElementById('navbarNav');
+
+humbergIcon.addEventListener('click', () => {
+  navbarNav.style.width = '100%';
+  closeBtn.style.display = 'block';
+});
+closeBtn.addEventListener('click', () => {
+  navbarNav.style.width = '0%';
+  closeBtn.style.display = 'none';
+});
+const menuItems = document.getElementsByClassName('nav-link');
+Array.from(menuItems).forEach((li) => {
+  li.addEventListener('click', () => {
+    navbarNav.style.width = '0%';
+    closeBtn.style.display = 'none';
+  });
+});
+let speakersHtml = '';
 let count = 0;
 Object.values(speakers).forEach((speakers) => {
-    
-    if(count == 0){
-        speakers_html +=`<div class="row mt-5">`
-    }else if(count === 2 || count === 4){
-        speakers_html += `</div><div class="row mt-5">`
-    }
-    speakers_html +=`<div class="col-md col-12">
+  if (count === 0) {
+    speakersHtml += '<div class="row mt-5">';
+  } else if (count === 2 || count === 4) {
+    speakersHtml += '</div><div class="row mt-5">';
+  }
+  speakersHtml += `<div class="col-md col-12">
                         <div class="row">
                             <div class="col-4 parent_div">
-                            <img src="${speakers.chess_icon}" alt="" class="chess first_chess">
+                            <img src="${speakers.chessIcon}" alt="" class="chess first_chess">
                             <img src="${speakers.image}" alt="" class="user custom_height w-100 "></div>
                             <div class="col-8 justify-content-center">
                             <h2 class="d-block">${speakers.name}</h2>
@@ -64,22 +81,19 @@ Object.values(speakers).forEach((speakers) => {
                             </div>
                         </div>
                     </div>`;
-    if(count === 5){
-        speakers_html +=`</div>`;
-    }                
-    count++;
-    
-})
-let speaker_head_html =` <div class="container p-3 mb-5">
+  if (count === 5) {
+    speakersHtml += '</div>';
+  }
+  count += 1;
+});
+let speakerHeadHtml = ` <div class="container p-3 mb-5">
                             <h3 id="program_heading" class="text-center">Featured Speakers</h3>
                             <hr class="hr">
 
                             </div>
 
                             <div class="container">`;
-speaker_head_html += speakers_html;
-speaker_head_html +=`</div>`;
-const speakers_section = document.getElementById('speakers');
-console.log(speakers_section);
-speakers_section.innerHTML = speaker_head_html;                       
-
+speakerHeadHtml += speakersHtml;
+speakerHeadHtml += '</div>';
+const speakersSection = document.getElementById('speakers');
+speakersSection.innerHTML = speakerHeadHtml;
